db841e69e607993127fe1c0c7bb8b1741154dab4
Bill Muzika <bill.muzika@outlook.com>
2021-12-02 09:03:11 -0500

    Pull some generic helpers out of day1

---
+++ AoC_helpers/src/lib.rs
@@ -0,0 +1,44 @@
+use std::fs::File;
+use std::io::Read;
+
+pub fn open_file_into_string(path: &str) -> String {
+    let mut file = match File::open(path) {
+        Ok(file) => file,
+        Err(_) => panic!("no file"),
+    };
+
+    let mut file_contents = String::new();
+    file.read_to_string(&mut file_contents)
+        .ok()
+        .expect("failed to read");
+
+    file_contents
+}
+
+pub fn split_by_newline(input: String) -> Vec<String> {
+    let result = input.split('\n')
+        .map(|s| s.to_string())
+        .filter(|s| s != "")
+        .collect::<Vec<String>>();
+
+    result
+}
+
+pub fn string_into_vec_of_uint(input: String) -> Vec<u64> {
+    let result = input.split('\n')
+                      .map(|s| s.to_string())
+                      .filter(|s| s != "")
+                      .map(|s| s.parse::<u64>().unwrap())
+                      .collect::<Vec<u64>>();
+
+    result
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,39 +1,12 @@
-use std::fs::File;
-use std::io::Read;
-
 fn main() {
-    let input = open_file_into_string("./input.txt");
-    let input = aoc_string_into_vec_of_uint(input);
+    let input = AoC_helpers::open_file_into_string("./input.txt");
+    let input = AoC_helpers::string_into_vec_of_uint(input);
 
     let part1_result = process_part_one(&input);
     let part2_result = process_part_two(&input, 3);
 
     println!("Part 1 Result = {}", part1_result);
     println!("Part 2 Result = {}", part2_result)
-}
-
-fn open_file_into_string(path: &str) -> String {
-    let mut file = match File::open(path) {
-        Ok(file) => file,
-        Err(_) => panic!("no file"),
-    };
-
-    let mut file_contents = String::new();
-    file.read_to_string(&mut file_contents)
-        .ok()
-        .expect("failed to read");
-
-    file_contents
-}
-
-fn aoc_string_into_vec_of_uint(input: String) -> Vec<u64> {
-    let result = input.split('\n')
-        .map(|s| s.to_string())
-        .filter(|s| s != "")
-        .map(|s| s.parse::<u64>().unwrap())
-        .collect::<Vec<u64>>();
-
-    result
 }
 
 fn process_part_one(input: &Vec<u64>) -> u16 {
